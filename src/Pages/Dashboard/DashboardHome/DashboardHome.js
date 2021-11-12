b63399654202b0aaee2978a24b5852a0a86cb556
CoderMahabub <codermahabub@gmail.com>
2021-11-13 05:21:44 +0600

    Readme.md file updated

--- src/Pages/Dashboard/DashboardHome/DashboardHome.js
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 
 const DashboardHome = () => {
+
     return (
-        <div>
-            <h1>Dashboard Home Total order morder thakbe total user egula bal sal ja ja ase</h1>
-        </div>
+        <>
+            <Typography variant="h2" display="block" gutterBottom>
+                Welcome to Dashboard
+            </Typography>
+            <Grid container sx={{
+                display: 'grid',
+                columnGap: 1,
+                rowGap: 1,
+                gridTemplateColumns: 'repeat(4, 1fr)',
+            }} spacing={2}>
+                <Grid item sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'yellow', alignItems: 'center', py: 3, px: 5 }}>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        Total Users
+                    </Typography>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        1254
+                    </Typography>
+                </Grid>
+                <Grid item sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'yellow', alignItems: 'center', py: 3, px: 5 }}>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        Total Users
+                    </Typography>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        1254
+                    </Typography>
+                </Grid>
+                <Grid item sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'yellow', alignItems: 'center', py: 3, px: 5 }}>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        Total Users
+                    </Typography>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        1254
+                    </Typography>
+                </Grid>
+                <Grid item sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'yellow', alignItems: 'center', py: 3, px: 5 }}>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        Total Users
+                    </Typography>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        1254
+                    </Typography>
+                </Grid>
+            </Grid >
+        </>
     );
 };
 
