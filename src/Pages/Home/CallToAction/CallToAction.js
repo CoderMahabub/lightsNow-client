e4f7377e40f2d0734c0314ea4175a141a16c2b10
CoderMahabub <codermahabub@gmail.com>
2021-12-09 08:50:44 +0600

    Retouch

--- src/Pages/Home/CallToAction/CallToAction.js
+++ src/Pages/Home/CallToAction/CallToAction.js
@@ -7,8 +7,8 @@ const CallToAction = () => {
     return (
         <div className="call-to-action d-flex align-items-center">
             <Container className=" text-light text-start">
-                <h1 className="fw-bold pb-3">End of season 2021 <br /> sale up to 30%</h1>
-                <p className="py-3">Stock is limited. Order now to avoid <br /> disappointment!</p>
+                <h1 className="fw-bold pb-3">End of season 2021 <br /> SALE up to 30%</h1>
+                <p className="py-3">Stock is limited. Order now to avoid <br /> disappointments!</p>
                 <NavLink to="/exploreLights">
                     <button className="btn btn-warning text-light">Explore Now</button>
                 </NavLink>
